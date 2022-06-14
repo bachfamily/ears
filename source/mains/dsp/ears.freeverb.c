5089cd65bedd2ea4453ff5a0cab51222956e0364
Andrea Agostini <and.agos@gmail.com>
2022-06-14 22:10:16 +0200

    freeverb ok

--- source/mains/dsp/ears.freeverb.c
+++ source/mains/dsp/ears.freeverb.c
@@ -206,7 +206,6 @@ void C74_EXPORT ext_main(void* moduleRef)
     class_register(CLASS_BOX, c);
     s_tag_class = c;
     ps_event = gensym("event");
-    return 0;
 }
 
 void buf_freeverb_assist(t_buf_freeverb *x, void *b, long m, long a, char *s)
