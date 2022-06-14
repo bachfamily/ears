05efb8e88b535e71fde5f54852672193761ce16a
Andrea Agostini <and.agos@gmail.com>
2022-06-14 21:56:43 +0200

    added wavpack library project

--- source/lib/WavPack/src/open_filename.c
+++ source/lib/WavPack/src/open_filename.c
@@ -27,7 +27,11 @@
 // local character set works. This is ignored on non-Windows platforms
 // (which is okay because they are probably UTF-8 anyway).
 
+#ifdef MAC_VERSION
 #include <unistd.h>
+#else
+#include <io.h>
+#endif
 
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
